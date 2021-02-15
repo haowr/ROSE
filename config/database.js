431a1c01175db84314174e8bd496f8631f32c6de
haowr <shinedesign@protonmail.com>
2021-02-15 00:02:46 -0800

    INITIAL-COMMIT

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports={
+
+ //database:'mongodb://localhost:27017/rose',
+database:'mongodb+srv://ohrha:2chuva2ed@rose.tt8t2.mongodb.net/rose?retryWrites=true&w=majority',
+
+ 
+ 
+     
+
+ secret:'mysecret'
+
+}
