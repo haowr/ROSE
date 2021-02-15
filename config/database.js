3e47c73b86442d69cada681d494463d90e72f0c3
haowr <shinedesign@protonmail.com>
2021-02-15 03:07:51 -0800

    fjdljf

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports={
 
  //database:'mongodb://localhost:27017/rose',
-database:'mongodb+srv://ohrha:2chuva2ed@rose.tt8t2.mongodb.net/rose?retryWrites=true&w=majority',
+//database:'mongodb+srv://ohrha:2chuva2ed@rose.tt8t2.mongodb.net/rose?retryWrites=true&w=majority',
+database:'mongodb://ohrha:tchuva2ed@ds133776.mlab.com:33776/rose',
 
  
  
